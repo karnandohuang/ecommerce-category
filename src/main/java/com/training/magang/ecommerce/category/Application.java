bb56b98424a0d469776e5bd6ca8de6748071cde2
karnandohuang <karnandohuang@outlook.com>
2019-03-13 15:42:28 +0700

    COMMIT-5 Added API Key Error 401 Exception

--- src/main/java/com/training/magang/ecommerce/category/Application.java
+++ src/main/java/com/training/magang/ecommerce/category/Application.java
@@ -1,10 +1,18 @@
 package com.training.magang.ecommerce.category;
 
+import com.training.magang.ecommerce.category.model.ApiKeyHandlerMethodArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebFluxConfigurer {
+
+	@Override
+	public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
+		configurer.addCustomResolver(new ApiKeyHandlerMethodArgumentResolver());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/training/magang/ecommerce/category/model/ApiKeyHandlerMethodArgumentResolver.java
+++ src/main/java/com/training/magang/ecommerce/category/model/ApiKeyHandlerMethodArgumentResolver.java
@@ -3,8 +3,11 @@ package com.training.magang.ecommerce.category.model;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.reactive.BindingContext;
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 public class ApiKeyHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
@@ -14,12 +17,16 @@ public class ApiKeyHandlerMethodArgumentResolver implements HandlerMethodArgumen
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String value = webRequest.getHeader("Api-Key");
+    public Mono<Object> resolveArgument(MethodParameter parameter, BindingContext bindingContext, ServerWebExchange exchange) {
+        return Mono.fromSupplier(() ->{
+            String value = exchange.getRequest().getHeaders().getFirst("Api-Key");
 
-        if(value == null){
-            throw new ApiKeyException("Oops, API KEY is Empty");
-        } else {
-            return new ApiKey(value);
-        }    }
+            if(value == null){
+                throw new ApiKeyException("Oops, API KEY is Empty");
+            } else {
+                return new ApiKey(value);
+            }
+        });    }
+
+
 }
